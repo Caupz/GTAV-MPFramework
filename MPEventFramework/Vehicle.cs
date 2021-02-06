a7496f358d8c6dc47a8f81e8e3b6ea1789260b28
Caupo Helvik <helvikcaupo@gmail.com>
2021-02-06 17:06:57 +0200

    Enhanced vehicle seats and functions

--- MPEventFramework/Vehicle.cs
+++ MPEventFramework/Vehicle.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
 
 namespace MPEventFramework
 {
-    class Vehicle
+    class Vehicle : BaseScript
     {
         public const int HANDLE_NONE = -1;
-        public const int SEAT_INDEX_NONE = -1;
-        public const int SEAT_INDEX_DRIVE = 0;
+
+        public const int SEAT_NONE = -2;
+        public const int SEAT_DRIVER = -1;
+        public const int SEAT_PASSENGER = 0;
+        public const int SEAT_BACK_LEFT_PASSENGER = 1;
+        public const int SEAT_BACK_RIGHT_PASSENGER = 2;
+        public const int SEAT_FURTHER_BACK_LEFT_PASSENGER = 3;
+        public const int SEAT_FURTHER_BACK_RIGHT_PASSENGER = 4;
+
+        public static int[] seats = new int[6] { SEAT_DRIVER, SEAT_PASSENGER, SEAT_BACK_LEFT_PASSENGER, SEAT_BACK_RIGHT_PASSENGER, SEAT_FURTHER_BACK_LEFT_PASSENGER, SEAT_FURTHER_BACK_RIGHT_PASSENGER };
+        // Could be bigger seat index also for example bus.
+
+        public static int GetMaxNumberOfSeats(int vehicleHandle)
+        {
+            return GetVehicleMaxNumberOfPassengers(vehicleHandle) + 1; // NOTE(Caupo 06.02.2021): +1 because the functions returns only passengers and does not include driver seat.
+        }
     }
 }
