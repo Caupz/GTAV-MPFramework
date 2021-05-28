7b14d98fde8b74b53cc1f87593ef077a2db0a310
Caupo Helvik <helvikcaupo@gmail.com>
2021-05-28 10:40:48 +0300

    Changed copyright text to MIT license

--- MPEventFramework/Properties/AssemblyInfo.cs
+++ MPEventFramework/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MPEventFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
